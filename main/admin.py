cd9da1c71458f2803c86ce064dae5ec555873594
Ali Hussein <ah0979075074@gmail.com>
2026-01-11 01:27:21 +0300

    configure admin.py to include hashed passwords in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(User)
 admin.site.register(Score)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
 

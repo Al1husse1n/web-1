909b576e24a6ad51214089003b3fff332128e206
Ali Hussein <ah0979075074@gmail.com>
2026-01-11 01:33:52 +0300

    add a redirect view

--- main/urls.py
+++ main/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('auth/', views.auth, name='auth'),
     path('profile/', views.profile, name="profile"),
     path('leaderboards/', views.leaderboards, name='leaderboards'),
     path('statistics/', views.statistics, name='statistics'),
     path('authenticate/' ,views.authentication, name='autnenticate'),
-]   
+    path('', RedirectView.as_view(url='/leaderboards/', permanent=True)),
+]         

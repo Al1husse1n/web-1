a15d6b8e2c1f0732eb87cb4c4dd30a0fd903174f
Ali Hussein <ah0979075074@gmail.com>
2026-01-29 18:35:56 +0300

    handle stat coming from home page

--- main/views.py
+++ main/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.contrib.auth import login,logout, authenticate
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 import json
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 from .random_api import param
 
+@login_required
 def leaderboards(request):
     time = request.GET.get('time')
     if time == 'all' or time == None: 
@@ -20,6 +22,7 @@ def leaderboards(request):
         scores = Score.objects.filter(time=60)
     return render(request, 'main/leaderboards.html',{"scores":scores})
 
+@login_required
 def home(request):
     return render(request, 'main/home.html')
 
@@ -59,13 +62,70 @@ def authentication(request):
     
 def profile(request):
     pass    
-def statistics(request):
-    return render(request, "main/stat.html")                
 
+@login_required
+@csrf_exempt
+def statistics(request):
+    if request.method != "POST":
+        return JsonResponse({"error":"only post requests are allowed"},status=405)
+    try:
+        current_user = request.user
+        data = json.loads(request.body)
+        wpm = data.get("wpm")
+        accuracy = data.get("accuracy")
+        statement = data.get("statement")
+        correct_words = data.get("correct_words")
+        incorrect_words = data.get("incorrect_words")
+        time = data.get("time")
+        category = data.get("category")
+        language = data.get("language")
+        score = Score(
+            wpm=wpm,
+            accuracy=accuracy,
+            statement=statement,
+            correct_words=correct_words,
+            incorrect_words=incorrect_words,
+            time=time,
+            user=current_user
+        )
+        score.full_clean()
+        score.save()
+        print(score)
+        return JsonResponse({"message":"score saved successfully"}, status = 200)
+    except ValidationError as e:
+        return JsonResponse({"errors":e.message_dict}, status=400)
+    except IntegrityError as e:
+        return JsonResponse({"error":str(e)}, status=400)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+
+@login_required
+def stat(request):
+    wpm = request.GET.get("wpm")
+    accuracy = request.GET.get("accuracy")
+    category = request.GET.get("category")
+    language = request.GET.get("language")
+    statement_length = request.GET.get("statement_length")
+    correct_words = request.GET.get("correct_words")
+    incorrect_words = request.GET.get("incorrect_words")
+    time = request.GET.get("time")
+    return render(request, "main/stat.html", {
+            "wpm":int(wpm),
+            "accuracy":int(accuracy),
+            "category":category,
+            "language":language,
+            "characters":int(statement_length), 
+            "correct_words":int(correct_words),
+            "incorrect_words":int(incorrect_words),
+            "time":int(float(time))
+        })
+
+@login_required
 def logout_view(request):
     logout(request)
     return redirect('auth')
 
+@login_required
 def statement(request):
     if request.method != "GET":
         return JsonResponse({"error": "only get requests are allowed"}, status=400)
